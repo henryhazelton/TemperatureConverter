e76e16fadcab5bc22b0f508e526e128da2c1032e
henryhazelton <henryhazelton00@gmail.com>
2025-11-21 16:26:02 +0000

    learning fundamentals of rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the temp converter, what do you need converting?");
+    println!("Do you want to convert (1) Celsius to Fahrenheit or (2) Fahrenheit to Celsius?");
+    let mut user_input = String::new();
+
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Failed to read user input");
+
+    println!("You chose: {}", user_input.trim());
+
+    match user_input.trim() {
+        "1" => println!("Converting Celsius to Fahrenheit"),
+        "2" => println!("Converting Fahrenheit to Celsius"),
+        _ => println!("Invalid choice!"),
+    }
 }
