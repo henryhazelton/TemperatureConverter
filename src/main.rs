819f6484dc583ef276cb9c5c0d3f79c4fe7cc855
henryhazelton <henryhazelton00@gmail.com>
2025-11-26 22:53:31 +0000

    building out the tool slowly

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,33 @@ use std::io;
 fn main() {
     println!("Welcome to the temp converter, what do you need converting?");
     println!("Do you want to convert (1) Celsius to Fahrenheit or (2) Fahrenheit to Celsius?");
-    let mut user_input = String::new();
+    let mut conversion_choice = String::new();
 
     io::stdin()
-        .read_line(&mut user_input)
+        .read_line(&mut conversion_choice)
         .expect("Failed to read user input");
 
-    println!("You chose: {}", user_input.trim());
+    println!("You chose: {}", conversion_choice.trim());
 
-    match user_input.trim() {
-        "1" => println!("Converting Celsius to Fahrenheit"),
+    match conversion_choice.trim() {
+        "1" => {
+            println!("Converting Celsius to Fahrenheit");
+            println!("Enter the temperature in Celcius you would like to convert:");
+            let mut temperature_input = String::new();
+            io::stdin()
+                .read_line(&mut temperature_input)
+                .expect("Failed to read temperature input");
+            let mut temperature_to_convert: f64 = temperature_input
+                .trim()
+                .parse()
+                .expect("Failed to convert temp string to float");
+            let fahrenheit_result = (temperature_to_convert * 2.0) + 30.0;
+            println!(
+                "{} Degrees Celcius to Fahrenheit is {} Fahrenheit",
+                temperature_input.trim(),
+                fahrenheit_result
+            );
+        }
         "2" => println!("Converting Fahrenheit to Celsius"),
         _ => println!("Invalid choice!"),
     }
